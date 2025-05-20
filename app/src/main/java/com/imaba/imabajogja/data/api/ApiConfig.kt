34e27f17e99cfd9139abc8c15a9f91a2761b70b6
Azrx <irvanea1@gmail.com>
2025-05-20 22:56:14 +0700

    logo
    Signed-off-by: Azrx <irvanea1@gmail.com>

--- app/src/main/java/com/imaba/imabajogja/data/api/ApiConfig.kt
+++ app/src/main/java/com/imaba/imabajogja/data/api/ApiConfig.kt
@@ -106,12 +106,13 @@ object ApiConfig {
     @Provides
     @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
-        val url = "http://192.168.100.7:8000/api/"
+//        val url = "http://192.168.100.7:8000/api/"
 //        val url = "http://192.168.31.44:8000/api/"
 //        val url = "http://192.168.54.254:8000/api/" //bld
 //        val url = "http://10.0.2.2:8000/api/"
 //        val url = "http://192.168.55.183:8000/api/"
-//        val url = "http://192.168.177.251:8000/api/"
+//        val url = "http://192.168.1.193:8000/api/" //mam
+        val url = "http://webservice.imabayogyakarta.com/api/" //mam
 
         val gson = GsonBuilder()
             .setLenient()
